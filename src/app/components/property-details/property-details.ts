8225b326869abae1f221739c9263bbfcd9042a62
andrewref <161936928+andrewref@users.noreply.github.com>
2025-08-22 22:13:24 +0300

    Angular setup

---
+++ src/app/components/property-details/property-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-details',
+  imports: [],
+  templateUrl: './property-details.html',
+  styleUrl: './property-details.scss'
+})
+export class PropertyDetails {
+
+}
